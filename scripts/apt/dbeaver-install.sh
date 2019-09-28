1d8d9228eab317a49ed0cbda8ac92cde81923b01
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2019-09-28 10:58:44 -0300

    fix dbeaver install

--- scripts/apt/dbeaver-install.sh
+++ scripts/apt/dbeaver-install.sh
@@ -6,4 +6,10 @@ then
     exit 1
 fi
 
-apt-get install --yes dbeaver-ce
+echo "deb https://dbeaver.io/debs/dbeaver-ce /" | sudo tee /etc/apt/sources.list.d/dbeaver.list
+
+wget -O - https://dbeaver.io/debs/dbeaver.gpg.key | sudo apt-key add -
+
+apt update
+
+apt install --yes dbeaver-ce
