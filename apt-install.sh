143e082f8dd5cd27efdd981d32a9742b8f631934
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2019-09-28 14:22:11 -0300

    add extensions and lm-sensors

--- apt-install.sh
+++ apt-install.sh
@@ -11,6 +11,8 @@ bash scripts/apt/chrome-install.sh
 bash scripts/apt/dbeaver-install.sh
 bash scripts/apt/docker-install.sh
 bash scripts/apt/fish-install.sh
+bash scripts/apt/gnome-shell-extensions-install.sh
+bash scripts/apt/lm-sensors-install.sh
 bash scripts/apt/spotify-install.sh
 bash scripts/apt/synaptic-install.sh
 bash scripts/apt/tig-install.sh
