c5298f8d522e69ec7eb7fd95a51c2c93d1d24c8e
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2019-09-28 11:14:24 -0300

    fix spotify install and clean unused packages

--- scripts/apt/pip3-install.sh
+++
@@ -1,9 +0,0 @@
-#!bin/bash
-
-if [ ! "`whoami`" = "root" ]
-then
-    echo "Use sudo to run this script"
-    exit 1
-fi  
-
-apt-get install --yes python3-pip
--- scripts/apt/spotify-install.sh
+++ scripts/apt/spotify-install.sh
@@ -6,4 +6,10 @@ then
     exit 1
 fi
 
-apt-get install --yes spotify-client
+curl -sS https://download.spotify.com/debian/pubkey.gpg | sudo apt-key add -
+
+echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+
+apt update
+
+apt install --yes spotify-client
