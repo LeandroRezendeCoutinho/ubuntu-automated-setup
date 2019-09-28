33b2632a8a82c58d627af6db883df2a7de912b75
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2019-09-28 10:41:46 -0300

    fix vscode install

--- scripts/apt/visual-studio-code-install.sh
+++ scripts/apt/visual-studio-code-install.sh
@@ -7,7 +7,11 @@ then
 fi
 
 curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > packages.microsoft.gpg
-install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
-sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
 
-apt install code
+sudo install -o root -g root -m 644 packages.microsoft.gpg /usr/share/keyrings/
+
+sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list'
+
+apt update
+
+apt install --yes code
