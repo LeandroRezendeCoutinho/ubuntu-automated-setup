fcd7a2784ac1185cc4562e30917f122e50e37b3c
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2019-10-04 09:27:03 -0300

    add ruby dev gems

--- scripts/setup/ruby-setup.sh
+++ scripts/setup/ruby-setup.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 
-asdf plugin-add ruby   
+asdf plugin-add ruby
 
 asdf install ruby 2.5.1
 asdf install ruby 2.6.3
@@ -8,3 +8,9 @@ asdf install ruby 2.6.3
 asdf global ruby 2.6.3
 
 gem install bundler
+gem install rubocop
+gem install reek
+gem install solargraph
+gem install fasterer
+gem install debride
+gem install ruby-lint
